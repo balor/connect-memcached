fe7ea0b08c4396ae8a8be39b0f281a5d512319ea
John Doe <john@ubuntu>
2014-05-12 23:09:36 -0700

    Testing that this works on express 4.xx

---
+++ tests/test.js
@@ -0,0 +1,31 @@
+var express      = require('express')
+, session        = require('express-session')
+, cookieParser   = require('cookie-parser')
+, http           = require('http')
+, app            = express()
+, MemcachedStore = require('../lib/connect-memcached')(session);
+
+app.use(cookieParser());
+app.use(session({
+      secret  : 'TestSecret'
+    , key     : 'test'
+    , proxy   : 'true'
+    , store   : new MemcachedStore({
+        hosts: [ '127.0.0.1:11211' ]
+    })
+}));
+
+app.get('/', function(req, res){
+    if(req.session.views) {
+        ++req.session.views;
+    } else {
+        req.session.views = 1;
+    }
+    res.send('Viewed <strong>' + req.session.views + '</strong> times.');
+});
+
+http.createServer(app).listen(9341, function() {
+    console.log("Listening on %d", this.address().port);
+});
+
+
