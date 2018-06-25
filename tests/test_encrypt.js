63afb657fc7c46fd27b4697b1fe42e2972022222
balor <balor@goto.hell>
2018-06-25 13:46:36 +0200

    test apps reformatted + added resave and saveUninitialized params

--- tests/test_encrypt.js
+++ tests/test_encrypt.js
@@ -1,33 +1,35 @@
-var express      = require('express')
-, session        = require('express-session')
-, cookieParser   = require('cookie-parser')
-, http           = require('http')
-, app            = express()
-, MemcachedStore = require('../lib/connect-memcached')(session);
+var express = require("express"),
+  session = require("express-session"),
+  cookieParser = require("cookie-parser"),
+  http = require("http"),
+  app = express(),
+  MemcachedStore = require("../lib/connect-memcached")(session);
 
 app.use(cookieParser());
-app.use(session({
-      secret  : 'TestEncryptSecret'
-    , key     : 'test_encrypt'
-    , proxy   : 'true'
-    , store   : new MemcachedStore({
-        hosts: [ '127.0.0.1:11211' ],
-        secret: 'Keep quiet, Im working!',
-        prefix: 'testapp_encrypt_'
+app.use(
+  session({
+    secret: "TestEncryptSecret",
+    key: "test_encrypt",
+    proxy: "true",
+    resave: false,
+    saveUninitialized: false,
+    store: new MemcachedStore({
+      hosts: ["127.0.0.1:11211"],
+      secret: "Keep quiet, Im working!",
+      prefix: "testapp_encrypt_"
     })
-}));
+  })
+);
 
-app.get('/', function(req, res){
-    if(req.session.views) {
-        ++req.session.views;
-    } else {
-        req.session.views = 1;
-    }
-    res.send('Viewed <strong>' + req.session.views + '</strong> times.');
+app.get("/", function(req, res) {
+  if (req.session.views) {
+    ++req.session.views;
+  } else {
+    req.session.views = 1;
+  }
+  res.send("Viewed <strong>" + req.session.views + "</strong> times.");
 });
 
 http.createServer(app).listen(9341, function() {
-    console.log("Listening on %d", this.address().port);
+  console.log("Listening on %d", this.address().port);
 });
-
-
