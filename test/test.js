fcc594246eac455022af5600365dedcd159db134
balor <balor@goto.hell>
2022-04-12 14:37:37 +0200

    TEST: created test which passes existing memcached client to store

--- test/test.js
+++ test/test.js
@@ -40,3 +40,24 @@ describe("View couter using encrypted memcached", () => {
     memcachedStore.client.end();
   });
 });
+
+describe("View couter using preexising encrypted memcached client", () => {
+  const { app, memcachedStore } = require("./services/memcached_preexisting_crypt_connection.js");
+  const serverAgent = supertest.agent(app);
+
+  it("GET / should increment views value at each request", async () => {
+    let lastPageView = 0;
+    for (var i = 1; i < 50; i++) {
+      const res = await serverAgent.get("/");
+      expect(res.status).toEqual(200);
+      expect(res.type).toEqual(expect.stringContaining("json"));
+      expect(res.body.pageviews).toBeGreaterThan(lastPageView);
+      expect(memcachedStore.crypto).toBe(require("crypto"));
+      lastPageView = res.body.pageviews;
+    }
+  });
+
+  afterAll(() => {
+    memcachedStore.client.end();
+  });
+});
